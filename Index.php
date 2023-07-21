8fc1b6df2af90dd602143089ea92377fd8c98b90
Naoufal69 <94354202+Naoufal69@users.noreply.github.com>
2023-07-21 12:06:14 +0200

    [MODIFICATION] - [Maj du style] - V.1.0.2

--- Index.php
+++ Index.php
@@ -46,6 +46,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Chiffrement et déchiffrement en PHP</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
     <link rel="icon" type="image/x-icon" href="./Images/Symbole.ico">
+    <script src="https://kit.fontawesome.com/f6fafc8c9a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 
 <body>
@@ -59,34 +61,55 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 
     <div class="mainContent">
-    <form method="post" action="">
-        <label for="key">Clé de chiffrement :</label><br>
-        <input type="text" name="key" id="key" value="<?php echo isset($key) ? $key : ''; ?>"><br>
-        <br>
-        <label for="plaintext">Texte à chiffrer :</label><br>
-        <textarea name="plaintext" id="plaintext" cols="40" rows="5"><?php echo isset($plaintext) ? $plaintext : ''; ?></textarea><br>
-        <input type="submit" name="encrypt" value="Chiffrer"><br>
-        <br>
-        <label for="ciphertext">Texte à déchiffrer :</label><br>
-        <textarea name="ciphertext" id="ciphertext" cols="40" rows="5"><?php echo isset($ciphertext) ? $ciphertext : ''; ?></textarea><br>
-        <input type="submit" name="decrypt" value="Déchiffrer"><br>
-    </form>
+        <form method="post" action="">
+            <label for="key">Clé de chiffrement :</label><br>
+            <input type="text" name="key" id="key" value="<?php echo isset($key) ? $key : ''; ?>"><br>
+            <br>
+            <label for="plaintext">Texte à chiffrer :</label><br>
+            <textarea name="plaintext" id="plaintext" cols="40" rows="5"><?php echo isset($plaintext) ? $plaintext : ''; ?></textarea><br>
+            <input type="submit" name="encrypt" value="Chiffrer"><br>
+            <br>
+            <label for="ciphertext">Texte à déchiffrer :</label><br>
+            <textarea name="ciphertext" id="ciphertext" cols="40" rows="5"><?php echo isset($ciphertext) ? $ciphertext : ''; ?></textarea><br>
+            <input type="submit" name="decrypt" value="Déchiffrer"><br>
+        </form>
 
-    <?php if (isset($encryptedText)) : ?>
-        <h2>Résultat du chiffrement :</h2>
-        <p><?php echo $encryptedText; ?></p>
-    <?php endif; ?>
+        
 
-    <?php if (isset($decryptedText)) : ?>
-        <h2>Résultat du déchiffrement :</h2>
-        <p><?php echo $decryptedText; ?></p>
-    <?php endif; ?>
 
-    <?php if (isset($error)) : ?>
-        <h2>Résultat du déchiffrement :</h2>
-        <p><?php echo $decryptedText; ?></p>
-    <?php endif; ?>
+
+        <?php if (isset($encryptedText)) : ?>
+            <h2>Résultat du chiffrement :</h2>
+            <div class="res"><p id="result"><?php echo $encryptedText; ?></p><button onclick="copyToClipboard()"><i class="fas fa-copy"></i></button></div>
+            
+        <?php endif; ?>
+
+        <?php if (isset($decryptedText)) : ?>
+            <h2>Résultat du déchiffrement :</h2>
+            <p id="result"><?php echo $decryptedText; ?></p>
+            <button onclick="copyToClipboard()"><i class="fas fa-copy"></i></button>
+        <?php endif; ?>
+
+        <?php if (isset($error)) : ?>
+            <h2>Résultat du déchiffrement :</h2>
+            <p><?php echo $decryptedText; ?></p>
+        <?php endif; ?>
     </div>
 </body>
+
+<script>
+    function copyToClipboard() {
+        const paragraphText = document.querySelector('#result').textContent;
+        const tempInput = document.createElement('textarea');
+        tempInput.value = paragraphText;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
+
+        alert('Le texte a été copié dans le presse-papiers.');
+    }
+</script>
 
 </html>
